08b7edbf4bd5109487a545779381acd92b2533a9
Phuong <lephuong10142002@gmail.com>
2026-02-08 19:57:10 +0700

    add dashboard

--- src/controllers/admin/dashboard.controller.ts
+++ src/controllers/admin/dashboard.controller.ts
@@ -1,7 +1,24 @@
 import { Request, Response } from "express";
 
 const getDashboardPage = (req: Request, res: Response) => {
-  return res.render("admin/dashboard.ejs");
+  return res.render("admin/dashboard/show.ejs");
 };
 
-export { getDashboardPage };
+const getAdminUserPage = (req: Request, res: Response) => {
+  return res.render("admin/user/show.ejs");
+};
+
+const getAdminProductPage = (req: Request, res: Response) => {
+  return res.render("admin/product/show.ejs");
+};
+
+const getAdminOrderPage = (req: Request, res: Response) => {
+  return res.render("admin/order/show.ejs");
+};
+
+export {
+  getDashboardPage,
+  getAdminUserPage,
+  getAdminProductPage,
+  getAdminOrderPage,
+};
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,4 +1,9 @@
-import { getDashboardPage } from "controllers/admin/dashboard.controller";
+import {
+  getAdminOrderPage,
+  getAdminProductPage,
+  getAdminUserPage,
+  getDashboardPage,
+} from "controllers/admin/dashboard.controller";
 import express, { Express } from "express";
 
 const router = express.Router();
@@ -6,6 +11,9 @@ const router = express.Router();
 const webRoutes = (app: Express) => {
   // admin routes
   router.get("/admin", getDashboardPage);
+  router.get("/admin/user", getAdminUserPage);
+  router.get("/admin/product", getAdminProductPage);
+  router.get("/admin/order", getAdminOrderPage);
 
   app.use("/", router);
 };
